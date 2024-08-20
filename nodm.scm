8d00d9f2de4dea2936aec39d297457dbb78bb1af
cutmad <50112797+cutmad@users.noreply.github.com>
2024-08-20 18:36:16 +0300

    Update nodm.scm

--- nodm.scm
+++ nodm.scm
@@ -58,25 +58,7 @@
                      ;; terminal emulator
                      kitty alacritty foot
 		     ;;fonts ill move them to home later
-                     font-adobe-source-code-pro font-adobe-source-han-sans 
-                     font-adobe-source-sans-pro font-adobe-source-serif-pro
-		     font-anonymous-pro font-anonymous-pro-minus font-awesome
-		     font-bitstream-vera font-blackfoundry-inria font-abattis-cantarell
-		     font-cns11643 font-cns11643-swjz font-comic-neue font-culmus
-                     font-dejavu font-dosis font-dseg font-fantasque-sans font-fira-code
-		     font-fira-mono font-fira-sans font-fontna-yasashisa-antique        
-		     font-gnu-freefont  font-gnu-unifont font-go                        
-		     font-google-material-design-icons font-google-noto font-google-robo
-		     font-hack font-hermit font-ibm-plex font-inconsolata font-iosevka  
-		     font-iosevka-aile font-iosevka-etoile font-iosevka-slab            
-		     font-iosevka-term font-iosevka-term-slab font-ipa-mj-mincho        
-		     font-jetbrains-mono font-lato font-liberation                      
-		     font-linuxlibertine font-lohit font-meera-inimai font-mononoki     
-		     font-mplus-testflight  font-opendyslexic                           
-		     font-public-sans font-rachana font-sarasa-gothic font-sil-andika   
-		     font-sil-charis font-sil-gentium font-tamzen font-terminus         
-		     font-tex-gyre  font-un font-vazir font-wqy-microhei                
-		     font-wqy-zenhei                                                    
+                                             
 		     ;; for HTTPS access
                      nss-certs)
                     %base-packages))
