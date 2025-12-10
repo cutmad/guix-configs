fab1e7bcc531d47e08dc00c214b0d81de7ee0bec
cutmad <50112797+cutmad@users.noreply.github.com>
2025-12-10 12:05:19 +0300

    Update channels.scm

--- channels.scm
+++ channels.scm
@@ -22,13 +22,4 @@
   (make-channel-introduction
    "7677db76330121a901604dfbad19077893865f35"
    (openpgp-fingerprint
-    "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7")))))
-(channel
-  (name 'home-service-dwl-guile)
-  (url "https://github.com/engstrand-config/home-service-dwl-guile")
-  (branch "main")
-  (introduction
-    (make-channel-introduction
-      "314453a87634d67e914cfdf51d357638902dd9fe"
-      (openpgp-fingerprint
-        "C9BE B8A0 4458 FDDF 1268 1B39 029D 8EB7 7E18 D68C"))))
+    "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))))
